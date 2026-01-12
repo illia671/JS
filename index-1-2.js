a49405bfddcca86829c165ad0152ec07f84a171f
illia671 <illiayakimov2011@email.com>
2026-01-12 14:40:35 +0200

    Initial commit

---
+++ index-1-2.js
@@ -0,0 +1,54 @@
+"use strict"
+
+// let str = "Hello"
+// let num = 123
+// let flag = false
+// let txt = "true"
+
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(typeof flag);
+// console.log(typeof txt);
+
+// const ageMe = 13
+// const ageFriend =  55
+// console.log(ageMe + ageFriend)
+// console.log(ageMe * ageFriend)
+
+// let course = prompt("Який курс ви вивчаєте?")
+// console.log("Я вивчаю курс ", course);
+// let name = prompt("what is your product name?")
+// let price = Number(prompt("what is your product price?"))
+// // alert("The product you choose ", name, " price is ", price, " USD")
+// console.log("The product you choose ", name, " price is ", price, " USD");
+// alert("Новорічні знижки на всі товари!")
+// console.log(typeof price)
+// const newPrice = price * 0.6 
+// console.log("твоя ціна зі знижкою ", newPrice, " USD")
+
+// магазин зілля
+// const qountityPotion = prompt("Скільки зілля ви хочете купити?")
+// const pricePotion = Number(prompt("Яка ціна за одне зілля?"))
+// const summ = qountityPotion * pricePotion
+// console.log(summ)
+// console.log(typeof summ);
+// console.log(typeof qountityPotion);
+
+// let wakeUp = Number(promt("О котрій годині ви зазвичай прокидаєтесь?"))
+// let wentToBed = Number(promt("О котрій годині ви зазвичай лягаєте спати?"))
+// const activeHours = wentToBed - wakeUp
+// const nightHours = 24 - activeHours
+// console.log("Ви активні протягом ", activeHours, " годин на добу")
+// console.log("Ви спите протягом ", nightHours, " годин на добу")
+
+
+// const name = prompt("ім'я персонажа")
+// const power = prompt("супер сила персонажа")
+// const cloak = confirm("чи є у персонажа плащ?")
+
+// console.log(`твій персонаж ${name} має супер силу ${power}. Чи є у нього плащ? - ${cloak}`);
+
+// магазин печева
+// const favoriteMusic = prompt("Яка твоя улюблена музика?")
+// const singer = prompt("Хто його виконавець?");
+// const year = Number(prompt("В якому році ця пісня вийшла?"))
---
+++ index1-1.js
@@ -0,0 +1,35 @@
+
+// const PASSWORD_USER = "0001111111123"; //константа - змінна яка не змінюється
+
+// console.log("Hi");
+
+// // const - зміна яка ну змінюється
+// //let - змінна яка може змінюватися
+// //var - застарілий спосіб оголошення змінних
+// //lala = 15 //глобальна змінна (погана практика)
+
+// const avarageValue = 10;
+
+// //ТИП ДАННИХ
+// //рядок string "55" 'gggg'
+// //NUMBER чимсла цілі, p rhfgrj. 10.5 111
+
+// console.log(1 + 1);
+// console.log("1" + "1");
+
+// //boolean
+// console.log(5 > 6);
+
+// //null - НІЩО
+// //undefined
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+// // alert(typeof name);
+
+// let name = prompt('Ваше ім"я?')
+// console.log(name)
+
+// let isAdult = confirm('Вам є 18 років?')
+// console.log(isAdult);
